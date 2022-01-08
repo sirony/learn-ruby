c35f99a33f81eacbf21ec772045131357c537fbe
Md. Shariful Islam <shariful_25@yahoo.com>
2022-01-08 15:31:23 +0600

    Create hello.rb
    This is very first program of ruby

---
+++ hello.rb
@@ -0,0 +1,13 @@
+=begin
+print and puts
+=end
+
+#The puts method display any string with new line
+
+puts "   /|"
+puts " /  |"
+puts "/   |"
+puts "----"
+
+#But print method only display the string output without newline
+print "End of program"
